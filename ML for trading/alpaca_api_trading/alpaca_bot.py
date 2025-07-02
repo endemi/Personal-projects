9814dfa24cf32f7bc11f6e6e51d3dae9331f1cf0
Alex Phan <105334704@student.swin.edu.au>
2025-07-02 15:36:26 +1000

    Current original

--- ML for trading/alpaca_api_trading/alpaca_bot.py
+++ ML for trading/alpaca_api_trading/alpaca_bot.py
@@ -8,3 +8,23 @@ client = TradingClient(config.API_KEY, config.SECRET_KEY, paper=True)
 account = dict(client.get_account())
 for k,v in account.items():
     print(f"{k}: {v}")
+
+
+
+order_details = MarketOrderRequest(
+    symbol="ETHUSD",
+    qty=10,
+    side=OrderSide.BUY,
+    time_in_force=TimeInForce.GTC  # <-- use GTC for crypto
+)
+
+order = client.submit_order(order_details)
+
+trades = TradingStream(config.API_KEY, config.SECRET_KEY, paper=True)
+
+async def trade_updates_handler(trade):
+    print(f"Trade update: {trade}")
+
+
+trades.subscribe_trade_updates(trade_updates_handler)
+trades.run()
