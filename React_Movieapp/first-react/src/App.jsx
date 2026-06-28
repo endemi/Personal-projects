689fad3d2ff6a4eb9bad5bd4e2208a7e7da1141d
Alex-Phan-swin <105334704@student.swin.edu.au>
2026-06-28 11:54:00 +1000

    States and hooks

--- React_Movieapp/first-react/src/App.jsx
+++ React_Movieapp/first-react/src/App.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react"
+
 const Card = ({title}) => {
+  const [hasLiked, setHasLiked] = useState(false);
+
   return (
     <div className="card">
       <h2>{title}</h2>
+
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? '❤️' : '💛'}
+      </button>
+
     </div>
   )
 }
 
 const App = () => {
+  const [hasLiked, setHasLiked] = useState(false);
+
+
   return (
   <div className="card-container">
     <Card title="Star" rating={5} isCool={true}/>
