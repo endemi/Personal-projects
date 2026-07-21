609cfde6cca53327512980407b4a23fa5639156d
Alex-Phan-swin <105334704@student.swin.edu.au>
2026-07-21 15:25:08 +1000

    Create contact route

--- Full Stack APP/backend/main.py
+++ Full Stack APP/backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import  jsonify, request
 from config import app, db
 from models import Contact
 
@@ -8,6 +8,24 @@ def get_contacts():
     contacts = Contact.query.all()
     json_contacts = list(map(lambda x: x.to_json(), contacts))
     return jsonify({'contacts': json_contacts})
+
+@app.route('/create_contact', methods=['POST'])
+def create_contact():
+    first_name = request.json.get['firstName']
+    last_name = request.json.get['lastName']
+    email = request.json.get['email']
+
+    if not first_name or not last_name or not email:
+        return (jsonify({'message': 'Missing required Fields'}), 400)
+    
+    new_contact = Contact(first_name=first_name, last_name=last_name, email=email)
+    try:
+        db.session.add(new_contact)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({'message': str(e)}), 400
+
+    return jsonify({'message': 'User Creeated'}), 201
 
 if __name__ == '__main__':
     with app.app_context():
